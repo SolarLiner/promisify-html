03c4f15c4818340e6949c41bce55f4e40d4f2303
Nathan Graule <solarliner@gmail.com>
2018-08-13 12:35:48 +0200

    Move extension interfaces to the source file directly

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
 type HTMLEventMap = WindowEventMap | DocumentEventMap | HTMLElementEventMap;
 
+interface Window {
+  on<K extends keyof WindowEventMap>(
+    event: K,
+    options?: AddEventListenerOptions
+  ): Promise<WindowEventMap[K]>;
+}
+
+interface Document {
+  on<K extends keyof DocumentEventMap>(
+    event: K,
+    options?: AddEventListenerOptions
+  ): Promise<DocumentEventMap[K]>;
+}
+
+interface HTMLElement {
+  on<K extends keyof HTMLElementEventMap>(
+    event: K,
+    options?: AddEventListenerOptions
+  ): Promise<HTMLElementEventMap[K]>;
+}
+
 function eventPromise<K extends keyof HTMLEventMap>(
   this: Window | Document | HTMLElement,
   event: K,
